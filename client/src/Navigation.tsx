50af67a706e7b1a87c04ecd83b7bea8a2fbfa2be
Dan Harris <danharris1606@gmail.com>
2018-10-17 20:24:59 +0100

    Create Navigation Component
    Added react-bootstrap as a dependency to use bootstrap components.
    Contains placeholder links for main pages.
    
    Also remove unneeded files from create-react-app script.

---
+++ client/src/Navigation.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+import { Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap';
+
+class Navigation extends React.Component {
+    public render() {
+        return (
+          <div className="App">
+            <Navbar>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="/">Project Manager</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav>
+                  <NavItem href="#">
+                    My Projects
+                  </NavItem>
+                  <NavItem href="#">
+                    Projects
+                  </NavItem>
+                </Nav>
+                <Nav pullRight={true}>
+                  <NavDropdown title="User" id="user-dropdown">
+                    <NavItem href="#">
+                      My Profile
+                    </NavItem>
+                    <NavItem href="#">
+                      Settings
+                    </NavItem>
+                    <NavItem href="#">
+                      Logout
+                    </NavItem>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+
+          </div>
+        );
+      }
+}
+
+export default Navigation;
