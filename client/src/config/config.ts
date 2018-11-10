b437fa29ec6265a5458a02cb0018338a1243ccae
Dan Harris <danharris1606@gmail.com>
2018-11-10 16:12:42 +0000

    Change from dev to prod in serverless and update endpoint base url

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: 'https://egz34v37if.execute-api.eu-west-2.amazonaws.com/dev',
+    BASE_URL: 'https://w0whqgrun9.execute-api.eu-west-2.amazonaws.com/prod',
     LOGIN_URL: 'https://dmh2g16-projectmanager-2.auth.eu-west-2.amazoncognito.com/login?response_type=token&client_id=4e84a2d32amefl6j3qthl4gj64',
 };
 
